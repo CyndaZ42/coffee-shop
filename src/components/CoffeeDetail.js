46bf5930626a61c8f40cb9f2649910438d9d9c9c
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-11-18 10:57:40 -0800

    Update CoffeeDetail.js

--- src/components/CoffeeDetail.js
+++ src/components/CoffeeDetail.js
@@ -7,9 +7,18 @@ function CoffeeDetail(props){
   return (
     <React.Fragment>
       <h1>Coffee Detail's</h1>
+      <h3>{coffee.name} Coffee - From {coffee.origin}</h3>
+      <p>Price: {coffee.price}</p>
+      <p>Roast: {coffee.roast}</p>
+      <p>Inventory: {coffee.inventory}</p>
+      <hr/>
       <hr/>
     </React.Fragment>
   );
 }
+
+CoffeeDetail.propTypes = {
+  coffee: PropTypes.object,
+};
 
 export default CoffeeDetail;
