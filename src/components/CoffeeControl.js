fa55219c5651c68206946bd2493b94b1cd214f94
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-11-18 11:04:46 -0800

    fix coffee control spelling error

---
+++ src/components/CoffeeControl.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class CoffeeControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      mainCoffeeList: [],
+      selectedCoffee: null,
+      editing: false
+    };
+  }
+  
+  handleClick = () => {
+    if (this.state.selectedTea != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTea: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null; 
+
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button> 
+      </React.Fragment>
+    );
+  }
+}
+
+export default CoffeeControl;
