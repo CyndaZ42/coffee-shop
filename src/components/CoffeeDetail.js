39d91322347545189537c25c3459d7fcf267d850
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-11-18 10:51:01 -0800

    Create CoffeeDetail.js

---
+++ src/components/CoffeeDetail.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function CoffeeDetail(props){
+  const { coffee } = props; 
+
+  return (
+    <React.Fragment>
+      <h1>Coffee Detail's</h1>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default CoffeeDetail;
