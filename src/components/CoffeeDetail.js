8a3e6ab83a47640a6bf7d7b0fb26491d5f058edb
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-11-18 15:20:05 -0800

    Update CoffeeDetail.js

--- src/components/CoffeeDetail.js
+++ src/components/CoffeeDetail.js
@@ -2,16 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function CoffeeDetail(props){
-  const { coffee } = props; 
+  const { coffee, onClickingDelete, onRestockClick } = props; 
 
   return (
     <React.Fragment>
-      <h1>Coffee Detail's</h1>
-      <h3>{coffee.name} Coffee - From {coffee.origin}</h3>
       <p>Price: {coffee.price}</p>
       <p>Roast: {coffee.roast}</p>
       <p>Inventory: {coffee.inventory}</p>
-      <hr/>
+
+      <button onClick={()=> onRestockClick()}>Restock Coffee</button>
+      <button onClick={props.onBuyClick}>Buy Coffee</button>
+      <button onClick={props.onClickingEdit}>Edit Coffee</button>
+      <button onClick={()=> onClickingDelete(coffee.id)}>Delete Coffee</button>
       <hr/>
     </React.Fragment>
   );
@@ -19,6 +21,10 @@ function CoffeeDetail(props){
 
 CoffeeDetail.propTypes = {
   coffee: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func,
+  onRestockClick: PropTypes.func,
+  onBuyClick: PropTypes.func
 };
 
 export default CoffeeDetail;
